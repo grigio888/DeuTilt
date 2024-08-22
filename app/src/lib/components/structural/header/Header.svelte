417193f2e882e572d28024a673329867ced34ff3
grigio888 <vinicius_grigio@hotmail.com>
2024-08-22 14:33:20 +0100

    Created a Header and fixed scss error.

---
+++ app/src/lib/components/structural/header/Header.svelte
@@ -0,0 +1,18 @@
+<header>
+    Example of header
+</header>
+
+<style lang="scss" scoped>
+    header {
+        position: sticky;
+        top: 0;
+
+        width: 100%;
+
+        padding: .5em 1em;
+
+        border-bottom: var(--border-width) solid var(--color-theme-1);
+
+        z-index: 5000; // 5000 is the default z-index of the header component
+    }
+</style>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -4,8 +4,12 @@
 
     // »»»»» Components
     import Metadata from '$comp/structural/Metadata.svelte';
+
+    import Header from '$comp/structural/header/Header.svelte';
 </script>
 
 <Metadata title="Pagina Inicial" description="Descrição da página inicial" keywords={['palavra1', 'palavra2']} />
+
+<Header />
 
 <h1>{_("Welcome to SvelteKit")}</h1>
