b5819dab6e0f4259e78d0d84c654231395b7aa49
grigio888 <vinicius_grigio@hotmail.com>
2024-08-22 19:33:09 +0100

    Changed app name and all its references

--- app/src/lib/components/structural/Metadata.svelte
+++ app/src/lib/components/structural/Metadata.svelte
@@ -22,31 +22,31 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-	<title>O Tilt{title != '' ? ' - ' + title : ''}</title>
+	<title>Deu Tilt{title != '' ? ' - ' + title : ''}</title>
 
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
-	<meta name="apple-mobile-web-app-title" content="O Tilt" />
+	<meta name="apple-mobile-web-app-title" content="Deu Tilt" />
 	<link rel="apple-touch-icon" href="{PUBLIC_APP_URL}/{image}" />
 	<meta name="apple-touch-fullscreen" content="yes" />
 
 	<meta property="og:description" content={description} />
 	<meta property="og:image" content="{PUBLIC_APP_URL}/{image}" />
-	<meta property="og:site_name" content="O Tilt" />
-	<meta property="og:title" content="O Tilt{title != '' ? ' - ' + title : ''}" />
+	<meta property="og:site_name" content="Deu Tilt" />
+	<meta property="og:title" content="Deu Tilt{title != '' ? ' - ' + title : ''}" />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="{PUBLIC_APP_URL}{page_url}" />
 
 	<meta name="description" content={description} />
-	<meta name="keywords" itemprop="keywords" content="{_('O Tilt')}, {keywords.toString()}" />
+	<meta name="keywords" itemprop="keywords" content="{_('Deu Tilt')}, {keywords.toString()}" />
 	<link rel="shortcut icon" href="{PUBLIC_APP_URL}/{image}" />
 	<link rel="icon" href="{PUBLIC_APP_URL}/{image}" />
 	<link rel="canonical" href="{PUBLIC_APP_URL}{page_url}" />
 
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:site" content="@o_tilt" />
-	<meta name="twitter:creator" content="@o_tilt" />
-	<meta name="twitter:title" content="O Tilt{title != '' ? ' - ' + title : ''}" />
+	<meta name="twitter:site" content="@deu_tilt" />
+	<meta name="twitter:creator" content="@deu_tilt" />
+	<meta name="twitter:title" content="Deu Tilt{title != '' ? ' - ' + title : ''}" />
 	<meta name="twitter:description" content={description} />
 	<meta name="twitter:image" content="{PUBLIC_APP_URL}/{image}" />
 </svelte:head>
