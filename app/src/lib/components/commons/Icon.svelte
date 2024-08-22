0a017c5515f6132877963287f62e7de951363b7a
grigio888 <vinicius_grigio@hotmail.com>
2024-08-22 19:13:49 +0100

    Created Icon component to handle Tabler icons.

---
+++ app/src/lib/components/commons/Icon.svelte
@@ -0,0 +1,16 @@
+<script>
+    // »»»»» Props
+    export let icon = 'question-mark';
+
+    // optional props
+    export let title = undefined;
+    export let customStyle = undefined;
+</script>
+
+<i class="ti ti-{icon}" {title} style="{customStyle}"></i>
+
+<style>
+    i {
+        font-size: 1.25em;
+    }
+</style>
