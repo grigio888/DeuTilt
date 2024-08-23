d0e0e3e6c9450feb593f2529c925738eb0a4b106
grigio888 <vinicius_grigio@hotmail.com>
2024-08-23 21:46:39 +0100

    Optimized user authentication.

--- app/src/hooks.server.js
+++ app/src/hooks.server.js
@@ -1,10 +1,14 @@
+import { PUBLIC_COOKIE_SECURE } from '$env/static/public';
+
 import User from '$db/models/User';
 import { locale } from '$i18n/store.js';
+
 
 export async function handle({ event, resolve }) {
 	// »»»»» Locals
 	// create a locals object if it doesn't exist
 	event.locals = event.locals ?? {};
+
 
 	// »»»»» Language
 	// configuring the language
@@ -14,16 +18,23 @@ export async function handle({ event, resolve }) {
 
 	locale.set(event.locals.lang);
 
+
     // »»»»» User
+    // configuring the user
     event.locals.user = {};
 
     let user = event.cookies.get('user');
     if (user) {
         event.locals.user = await User.findOne({ where: { password: user } });
+
+        if (!event.locals.user) {
+            event.cookies.delete('user', { path: '/', secure: JSON.parse(PUBLIC_COOKIE_SECURE) });
+        }
     }
 
-	// --------- RENDERING --------- //
-	// Transforming the page chunk
+
+	// »»»»» Rendering
+	// transforming the page chunk
 	return resolve(event, {
 		transformPageChunk: ({ html }) => html.replace('%lang%', event.locals.lang)
 	});
