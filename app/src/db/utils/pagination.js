39371104b066e1c953f67281cdd105cbab24629e
grigio888 <vinicius_grigio@hotmail.com>
2024-08-25 02:09:26 +0100

    Added a helper pagination on db/utils

---
+++ app/src/db/utils/pagination.js
@@ -0,0 +1,30 @@
+export default async function paginate({ model, page = 1, pageSize = 10, where = {}, order = [], returnAsJson = false }) {
+    let offset = (page - 1) * pageSize;
+    let limit = pageSize;
+
+    let { count, rows } = await model.findAndCountAll({
+        where,
+        order,
+        offset,
+        limit
+    });
+
+    let totalPages = Math.ceil(count / pageSize);
+
+    let hasPrev = page > 1;
+    let hasNext = page < totalPages;
+
+    if (returnAsJson) {
+        rows = rows.map((row) => row.toJSON());
+    }
+
+    return {
+        page,
+        pageSize,
+        totalPages,
+        totalItems: count,
+        hasPrev,
+        hasNext,
+        items: rows
+    };
+}
