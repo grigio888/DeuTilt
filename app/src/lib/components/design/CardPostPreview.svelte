0b6c66a24184631ed90a12a29605118778d5f3dc
grigio888 <vinicius_grigio@hotmail.com>
2024-08-25 02:47:02 +0100

    First iteration on Posts list.

---
+++ app/src/lib/components/design/CardPostPreview.svelte
@@ -0,0 +1,88 @@
+<script>
+	// »»»»» Props
+	let { item } = $props();
+
+	// »»»»» Components
+	import Tags from '$comp/design/Tags.svelte';
+</script>
+
+<div class="card">
+	<a href="/posts/{item.slug}">
+		<img src={item.imageHeader} alt={item.title} />
+	</a>
+	<div class="tags">
+		{#each item.Tags as tag}
+			<Tags {tag} />
+		{/each}
+	</div>
+	<a href="/posts/{item.slug}">
+		<h2>{item.title}</h2>
+	</a>
+</div>
+
+<style lang="scss">
+	.card {
+		border: var(--border-width) solid var(--color-theme-1);
+		border-radius: var(--border-radius);
+
+		width: 100%;
+		aspect-ratio: 18/9;
+
+		a {
+			display: block;
+			height: 100%;
+			width: 100%;
+
+			overflow: hidden;
+
+			&:not(:last-child) {
+				border-bottom: var(--border-width) solid var(--color-theme-1);
+			}
+
+			img {
+				object-fit: cover;
+				height: 100%;
+				width: 100%;
+
+				transition: transform 0.5s;
+
+				&:hover {
+					transform: scale(1.1);
+				}
+			}
+
+			h2 {
+				display: -webkit-box;
+				-webkit-box-orient: vertical;
+				-webkit-line-clamp: 5;
+
+				height: auto;
+				margin: 0.5em;
+				margin-top: 0;
+
+				font-size: 1.25rem;
+				text-overflow: ellipsis;
+
+				overflow: hidden;
+			}
+		}
+
+		.tags {
+			display: flex;
+			gap: 0.5em;
+			padding: 0.5em;
+		}
+	}
+
+	@media (max-width: 768px) {
+		.card {
+			aspect-ratio: 27/9;
+
+			a {
+				h2 {
+					-webkit-line-clamp: 3;
+				}
+			}
+		}
+	}
+</style>
--- app/src/routes/posts/+page.svelte
+++ app/src/routes/posts/+page.svelte
@@ -1,11 +1,34 @@
 <script>
+	// »»»»» Imports
+	import { translate as _ } from '$i18n/translate';
+
 	// »»»»» Props
 	let { data } = $props();
 	let { pagination } = data;
+
+	// »»»»» Components
+	import CardPostPreview from '$comp/design/CardPostPreview.svelte';
 </script>
 
-{#each pagination.items as item}
-	<a href="/posts/{item.slug}">
-		<h2>{item.title}</h2>
-	</a>
-{/each}
+<h1>{_('Arquivos')}</h1>
+
+<div class="wrapper">
+	{#each pagination.items as item}
+		<CardPostPreview {item} />
+	{/each}
+</div>
+
+<style lang="scss">
+	h1 {
+		margin: 1em;
+		text-align: center;
+	}
+
+	.wrapper {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+		gap: 1em;
+
+		margin: 1em;
+	}
+</style>
