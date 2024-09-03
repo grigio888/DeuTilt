5d39ceb4028d289a704baab909c1d377897fc6ca
grigio888 <vinicius_grigio@hotmail.com>
2024-09-03 11:17:31 +0100

    First iteration on login form

--- app/src/routes/auth/+page.svelte
+++ app/src/routes/auth/+page.svelte
@@ -1,6 +1,11 @@
 <script>
 	// »»»»» Imports
 	import { enhance } from '$app/forms';
+    import { translate as _ } from '$i18n/translate';
+
+    // »»»»» Components
+    import Input from '$comp/commons/inputs/Input.svelte';
+    import Button from '$comp/commons/buttons/Button.svelte';
 
 	// »»»»» Logic
 	let errorMessage = '';
@@ -24,7 +29,27 @@
 		};
 	}}
 >
-	<input type="text" name="username" placeholder="Username" />
-	<input type="password" name="password" placeholder="Password" />
-	<button type="submit">Login</button>
+    <label for="username">{_("Usuário")}</label>
+	<Input type="text" id="username" name="username" placeholder={_("Usuário")} />
+    <label for="password">{_("Senha")}</label>
+	<Input type="password" id="password" name="password" placeholder={_("Senha")} />
+	<Button type="submit">Login</Button>
 </form>
+
+<style lang="scss">
+    form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 1em;
+
+        height: 74vh;
+        width: 30em;
+
+        margin: auto;
+
+        > :global(button) {
+            width: 100%;
+        }
+    }
+</style>
