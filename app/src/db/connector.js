764ab861856698e6fe26dd1ca6e0f9609d5f1e4d
grigio888 <vinicius_grigio@hotmail.com>
2024-09-09 12:26:44 +0100

    Fixed Sequelize not using UTF-8 by default.

--- app/src/db/connector.js
+++ app/src/db/connector.js
@@ -3,7 +3,14 @@ import { Sequelize } from 'sequelize';
 let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
 	host: process.env.DB_HOST,
 	dialect: process.env.DB_DIALECT,
-	logging: false
+	logging: false,
+    define: {
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+    },
+    dialectOptions: {
+        charset: 'utf8',
+    },
 });
 
 export default sequelize;
