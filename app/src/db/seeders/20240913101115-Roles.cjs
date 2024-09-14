f97ad5f9af6f8d92c976d866615567d95cfaf96e
grigio888 <vinicius_grigio@hotmail.com>
2024-09-14 13:52:02 +0100

    Seeders must be a cjs file.

---
+++ app/src/db/seeders/20240913101115-Roles.cjs
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	// eslint-disable-next-line no-unused-vars
+    async up (queryInterface, Sequelize) {
+        await queryInterface.bulkInsert('Roles', [
+            {
+                slug: 'user',
+                title: 'Usuário',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                slug: 'moder',
+                title: 'Moderador',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                slug: 'admin',
+                title: 'Administrador',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ], {});
+    },
+
+	// eslint-disable-next-line no-unused-vars
+    async down (queryInterface, Sequelize) {
+        await queryInterface.bulkDelete('Roles', null, {});
+    }
+};
---
+++ app/src/db/seeders/20240913101452-Users.cjs
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	// eslint-disable-next-line no-unused-vars
+    async up (queryInterface, Sequelize) {
+        await queryInterface.bulkInsert('Users', [
+            {
+                username: 'admin',
+                password: '$2b$10$84H35EnW/c7jSygHnGPLkuuuOlaegpjDsCvwXMek4Ogt6DV6ae7Ji',
+                email: 'grigio888@deu-tilt.com',
+                firstName: 'Vinicius',
+                lastName: 'Grigio',
+                biography: null,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                roleId: 3
+            }
+        ], {});
+    },
+
+	// eslint-disable-next-line no-unused-vars
+    async down (queryInterface, Sequelize) {
+        await queryInterface.bulkDelete('Users', null, {});
+    }
+};
